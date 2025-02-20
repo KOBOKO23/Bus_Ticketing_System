0e83a8c7a86b729bb1be6976d45293b0a9eb6877
KOBOKO23 <kobokomasero@gmail.com>
2025-02-20 16:06:58 +0300

    config.py

--- config.py
+++ config.py
@@ -1,7 +1,9 @@
-MAIL_SERVER = 'smtp.gmail.com' 
-MAIL_PORT = 587                # Common port for TLS; 465 for SSL
-MAIL_USE_TLS = True            # Set True for TLS
-MAIL_USE_SSL = False           # Set True for SSL
-MAIL_USERNAME = 'kobokomasero@gmail.com'  # Replace with your email
-MAIL_PASSWORD = 'wzodhdkqwyltewom'  # Your generated app password (no spaces)
-MAIL_DEFAULT_SENDER = ('Bus_Reservation_System', 'kobokophilip@gmail.com')  # Default sender
+import os
+
+MAIL_SERVER = 'smtp.gmail.com'
+MAIL_PORT = 587
+MAIL_USE_TLS = True
+MAIL_USE_SSL = False
+MAIL_USERNAME = os.environ.get('MAIL_USERNAME')  # Get from environment variable
+MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')  # Get from environment variable
+MAIL_DEFAULT_SENDER = ('Bus_Reservation_System', os.environ.get('MAIL_DEFAULT_SENDER'))
